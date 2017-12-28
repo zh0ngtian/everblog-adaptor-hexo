68e99cc738ddc917577e58fbe9863bdef63ddefd
nswbmw <gxqzk@126.com>
2017-12-28 16:56:01 +0800

    Bump v0.2.0

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const _ = require('lodash')
 const fse = require('fs-extra')
 const moment = require('moment')
@@ -8,33 +6,31 @@ const entities = require('entities')
 const enml2text = require('enml2text')
 const debug = require('debug')('everblog-adaptor-hexo')
 
-module.exports = function* (data) {
+module.exports = async function (data, cleanMode = false) {
   const dist = process.cwd() + '/source/_posts/'
   fse.emptyDirSync(dist)
 
-  data.posts.forEach(post => {
+  data.notes.forEach(note => {
     const defaultFrontMatter = {
-      title: post.title,
-      date: formatDate(post.created),
-      updated: formatDate(post.updated),
-      tags: post.tags
+      title: note.title,
+      date: formatDate(note.created),
+      updated: formatDate(note.updated),
+      tags: note.tags
     }
-    debug('content -> %j', post.content)
+    debug(`title: ${note.title}, content(enml): ${note.content}`)
 
-    let contentMarkdown = entities.decodeHTML(enml2text(post.content))
-    debug('contentMarkdown -> %j', contentMarkdown)
-
+    let contentMarkdown = entities.decodeHTML(enml2text(note.content))
     let data = fm.parse(contentMarkdown)
     _.merge(data.attributes, defaultFrontMatter)
     contentMarkdown = fm.stringify(data)
 
-    const filename = dist + data.attributes.title + '.md'
+    const filename = dist + note.title + '.md'
     fse.outputFileSync(filename, contentMarkdown)
-    debug('title -> %s, body -> %j', data.attributes.title, contentMarkdown)
+    debug(`title: ${filename}, content(markdown): ${contentMarkdown}`)
   })
   debug('build success!')
 }
 
-function formatDate(timestamp) {
+function formatDate (timestamp) {
   return moment(timestamp).format('YYYY/M/DD HH:mm:ss')
 }
