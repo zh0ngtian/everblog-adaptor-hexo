11a84ff4d12d0ecbdd01aabbe8eff821c6aa0e95
YaoZhontian <305301890@qq.com>
2020-01-05 21:06:50 +0800

    distinguish ordinary note frome Markdown note

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const _ = require('lodash')
 const fse = require('fs-extra')
-const yaml = require('js-yaml')
+const parser = require('js-yaml')
 const moment = require('moment')
 const fm = require('front-matter')
 const entities = require('entities')
@@ -8,38 +8,146 @@ const enml2text = require('enml2text')
 const debug = require('debug')('everblog-adaptor-hexo')
 const urlencode = require('urlencode')
 
+var enml2html = require('enml2html') // use var for easy mock in mocha testing
+const cheerio = require('cheerio')
+const format = require('string-format')
+const Promise = require('bluebird')
+const path = require('path')
+const sanitize = require('sanitize-filename')
+const os = require('os')
+
 module.exports = async function (data, cleanMode = false) {
-  const configPath = process.cwd() + '/_config.yml'
-  fse.outputFileSync(configPath, yaml.safeDump(data.$blog))
-
-  const postsPath = process.cwd() + '/source/_posts/'
-  fse.emptyDirSync(postsPath)
-
-  data.posts.forEach(note => {
-    const defaultFrontMatter = {
-      title: note.title,
-      date: formatDate(note.created),
-      updated: formatDate(note.updated),
-      tags: note.tags
+  for(let note of data.posts) {
+    if (note.attributes.contentClass === 'yinxiang.markdown') {
+      processMarkdownNote(note)
+    } else {
+      note.webApiUrlPrefix = data.$webApiUrlPrefix
+      processOrdinaryNote(note)
+      sleep(5000)
     }
-    debug(`title: ${note.title}, content(enml): ${note.content}`)
-
-    let allContent = entities.decodeHTML(enml2text(note.content))
-    let allContentArray = allContent.split("\n")
-    let encodedContentMarkdown = allContentArray[allContentArray.length - 1]
-    let contentMarkdown = urlencode.decode(encodedContentMarkdown, 'utf-8')
-
-    let data = fm.parse(contentMarkdown)
-    _.merge(data.attributes, defaultFrontMatter)
-    contentMarkdown = fm.stringify(data)
-
-    const filename = postsPath + note.title + '.md'
-    fse.outputFileSync(filename, contentMarkdown)
-    debug(`title: ${filename}, content(markdown): ${JSON.stringify(contentMarkdown)}`)
-  })
-  debug('hexo build success!')
+  }
+  debug('build success!')
 }
 
-function formatDate (timestamp) {
+function processMarkdownNote(note) {
+  const defaultFrontMatter = {
+    title: note.title,
+    date: formatDate(note.created),
+    updated: formatDate(note.updated),
+    tags: note.tags
+  }
+
+  let allContent = entities.decodeHTML(enml2text(note.content))
+  let allContentArray = allContent.split("\n")
+  let encodedContentMarkdown = allContentArray[allContentArray.length - 1]
+  let contentMarkdown = urlencode.decode(encodedContentMarkdown, 'utf-8')
+
+  let data = fm.parse(contentMarkdown)
+  _.merge(data.attributes, defaultFrontMatter)
+  contentMarkdown = fm.stringify(data)
+
+  const filename = 'source/_posts/' + note.tags[0] + '@' + note.title + '.md'
+  fse.outputFileSync(filename, contentMarkdown)
+  debug(`title: ${filename}, content(markdown): ${JSON.stringify(contentMarkdown)}`)
+}
+
+function processOrdinaryNote(note) {
+  const defaultFrontMatter = {
+    title: note.title,
+    date: formatDate(note.created),
+    updated: formatDate(note.updated),
+    tags: note.tags
+  }
+
+  let contentMarkdown = enml2html(note)
+
+  let $ = cheerio.load(contentMarkdown)
+  const attributes = note.attributes
+  if (attributes) {
+    const sourceApplication = note.attributes.sourceApplication
+    if (sourceApplication && (sourceApplication === 'maxiang')) {
+      $('h1').remove()
+    }
+  }
+
+  // Download all images and update the src attribute.
+  if (note.resources) {
+    for(let res of note.resources) {
+      resolveNoteResource(res, note.tags[0] + '@' + note.title, $)
+    }
+  }
+
+  // longdesc and alt field will make the HTML show the picture name on page.
+  // That is not expected for some inline pictures.
+  // Just remove them.
+  $('img').attr('longdesc', '')
+  $('img').attr('alt', '')
+  // Originally, they are inline-block, which will make the view be out of page scope.
+  // Making it as block will force everything in scope.
+  // $('div').css('display', 'block')
+  contentMarkdown = $.html()
+  contentMarkdown = removeSpecialChar(contentMarkdown)
+
+  var info = fm(contentMarkdown)
+  _.merge(info.attributes, defaultFrontMatter)
+  contentMarkdown = fmStringify(info)
+
+  const dist = process.cwd() + '/source/_posts/'
+  const filename = (dist + info.attributes.tags[0] + '@' + info.attributes.title + '.html')
+  fse.outputFileSync(filename, contentMarkdown)
+  debug('title-> %s, content-> %s', info.attributes.tags[0] + '@' + info.attributes.title, contentMarkdown)
+}
+
+function resolveNoteResource(resData, title, html) {
+  let fileName = resData.attributes.fileName || Date.now().toString()
+  fileName = path.basename(fileName.replace(/_/g, ''))
+  const hash = bodyHashToString(resData.data.bodyHash)
+  const imgFile = format('/images/{}/{}', sanitize(title), fileName)
+  fse.outputFileSync(format('{}/source/{}', process.cwd(), imgFile), new Buffer(resData.data.body), 'binary')
+  html(format('img[hash="{}"]', hash)).attr('src', imgFile)
+}
+
+function removeSpecialChar(html) {
+  html = html.replace(/.*<!DOCTYPE/, '<!DOCTYPE')
+  html = html.replace(/{{/g, '&#123;&#123;')
+  return html.replace(/}}/g, '&#125;&#125;')
+}
+
+function formatDate(timestamp) {
   return moment(timestamp).format('YYYY/M/DD HH:mm:ss')
+}
+
+function bodyHashToString(bodyHash) {
+  let str = '';
+  for (let i in bodyHash) {
+    let hexStr = bodyHash[i].toString(16);
+    if (hexStr.length === 1) {
+      hexStr = '0' + hexStr;
+    }
+    str += hexStr;
+  }
+  return str;
+}
+
+function fmStringify (obj, opt) {
+  obj = obj || {}
+  opt = opt || {}
+  var attributes = obj.attributes || {}
+  var body = obj.body || {}
+  var scope = opt.scope || '---'
+
+  if (Object.keys(attributes).length === 0) {
+    return body
+  }
+
+  var yaml = parser.dump(attributes)
+  yaml = scope + os.EOL + yaml + scope + os.EOL + body
+
+  return yaml
+}
+
+function sleep(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
 }
